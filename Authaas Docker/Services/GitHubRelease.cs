13acf9ba4b942e40900a0443596bd28a2a1292ca
Stavros Giannis (DE) <stavros.giannis@pwc.com>
2023-08-03 10:41:40 +0200

    added summary

--- Authaas Docker/Services/GitHubRelease.cs
+++ Authaas Docker/Services/GitHubRelease.cs
@@ -2,115 +2,114 @@
 using System.Security.Cryptography;
 using System.Text.Json;
 
-namespace Authaas_Docker.Services
+namespace Authaas_Docker.Services;
+
+public class GitHubRelease
 {
-    public class GitHubRelease
+    private static readonly HttpClient Client = new();
+    public string? TagName { get; set; }
+
+    /// <summary>
+    ///     Calculates the current application hash.
+    /// </summary>
+    /// <returns>The current application hash.</returns>
+    public static string CalculateCurrentAppHash()
     {
-        private static readonly HttpClient Client = new();
-        public string? TagName { get; set; }
-
-        /// <summary>
-        ///     Calculates the current application hash.
-        /// </summary>
-        /// <returns>The current application hash.</returns>
-        public static string CalculateCurrentAppHash()
-        {
-            var location = Assembly.GetEntryAssembly()?.Location;
-            if (location != null)
-                using (var stream = File.OpenRead(location))
-                {
-                    using var sha = SHA1.Create();
-                    var hash = sha.ComputeHash(stream);
-                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-                }
-
-            return "";
-        }
-
-        /// <summary>
-        ///     Calculates the SHA1 hash of a file.
-        /// </summary>
-        /// <param name="filePath">The path of the file to calculate the hash for.</param>
-        /// <returns>The SHA1 hash of the file.</returns>
-        public static string CalculateFileHash(string filePath)
-        {
-            using (var stream = File.OpenRead(filePath))
+        var location = Assembly.GetEntryAssembly()?.Location;
+        if (location != null)
+            using (var stream = File.OpenRead(location))
             {
                 using var sha = SHA1.Create();
                 var hash = sha.ComputeHash(stream);
                 return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
             }
-        }
 
-        public static async Task DownloadLatestRelease(string repoOwner, string repoName, string tag,
-            ProgressBar progressBar)
+        return "";
+    }
+
+    /// <summary>
+    ///     Calculates the SHA1 hash of a file.
+    /// </summary>
+    /// <param name="filePath">The path of the file to calculate the hash for.</param>
+    /// <returns>The SHA1 hash of the file.</returns>
+    public static string CalculateFileHash(string filePath)
+    {
+        using (var stream = File.OpenRead(filePath))
         {
-            var url = $"https://github.com/{repoOwner}/{repoName}/releases/download/{tag}/authaas-docker.zip";
-
-            using var response = await Client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
-            response.EnsureSuccessStatusCode();
-
-            var contentLength = response.Content.Headers.ContentLength ?? 0;
-
-            using var stream = await response.Content.ReadAsStreamAsync();
-            using var fileStream = File.Create("AuthaasDocker.zip");
-
-            var totalBytesCopied = 0L;
-            var buffer = new byte[81920];
-            int bytesRead;
-            while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
-            {
-                await fileStream.WriteAsync(buffer, 0, bytesRead);
-                totalBytesCopied += bytesRead;
-
-                var progress = (int)(100L * totalBytesCopied / contentLength);
-                progressBar.Invoke(new Action(() => progressBar.Value = progress));
-            }
+            using var sha = SHA1.Create();
+            var hash = sha.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
         }
+    }
 
+    public static async Task DownloadLatestRelease(string repoOwner, string repoName, string tag,
+        ProgressBar progressBar)
+    {
+        var url = $"https://github.com/{repoOwner}/{repoName}/releases/download/{tag}/authaas-docker.zip";
 
-        public static async Task<GenericResult> DownloadLatestReleaseIfUpdateAvailable(Version? currentVersion,
-            string repoOwner,
-            string repoName, ProgressBar progressBar)
+        using var response = await Client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+
+        var contentLength = response.Content.Headers.ContentLength ?? 0;
+
+        using var stream = await response.Content.ReadAsStreamAsync();
+        using var fileStream = File.Create("AuthaasDocker.zip");
+
+        var totalBytesCopied = 0L;
+        var buffer = new byte[81920];
+        int bytesRead;
+        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
         {
-            var latestTag = await GetLatestReleaseTagAsync(repoOwner, repoName);
-            if (latestTag == null) throw new Exception("Could not retrieve latest release tag.");
+            await fileStream.WriteAsync(buffer, 0, bytesRead);
+            totalBytesCopied += bytesRead;
 
-            // Assuming that your tags are in the format "vX.Y.Z.X", we remove the leading 'v' 
-            // to convert the tag into a version string
-            var latestVersionStr = latestTag.TrimStart('v');
-            var latestVersion = new Version(latestVersionStr);
-
-            // Compare the current version with the latest version
-            if (currentVersion != null && currentVersion.CompareTo(latestVersion) < 0)
-            {
-                await DownloadLatestRelease(repoOwner, repoName, latestTag, progressBar);
-                return GenericResult.Ok("Downloading latest release..");
-            }
-
-            return GenericResult.Ok("Newest Version already!");
+            var progress = (int)(100L * totalBytesCopied / contentLength);
+            progressBar.Invoke(new Action(() => progressBar.Value = progress));
         }
+    }
 
 
-        /// <summary>
-        ///     Gets the latest release tag from a GitHub repository.
-        /// </summary>
-        /// <param name="repoOwner">The owner of the repository.</param>
-        /// <param name="repoName">The name of the repository.</param>
-        /// <returns>The latest release tag, or null if the repository does not exist.</returns>
-        public static async Task<string?> GetLatestReleaseTagAsync(string repoOwner, string repoName)
+    public static async Task<GenericResult> DownloadLatestReleaseIfUpdateAvailable(Version? currentVersion,
+        string repoOwner,
+        string repoName, ProgressBar progressBar)
+    {
+        var latestTag = await GetLatestReleaseTagAsync(repoOwner, repoName);
+        if (latestTag == null) throw new Exception("Could not retrieve latest release tag.");
+
+        // Assuming that your tags are in the format "vX.Y.Z.X", we remove the leading 'v' 
+        // to convert the tag into a version string
+        var latestVersionStr = latestTag.TrimStart('v');
+        var latestVersion = new Version(latestVersionStr);
+
+        // Compare the current version with the latest version
+        if (currentVersion != null && currentVersion.CompareTo(latestVersion) < 0)
         {
-            var url = $"https://api.github.com/repos/{repoOwner}/{repoName}/releases/latest";
-            Client.DefaultRequestHeaders.UserAgent
-                .ParseAdd("Authaas-Docker-Updater"); // GitHub API requires a User-Agent header
-            var json = await Client.GetStringAsync(url);
-            var release = JsonSerializer.Deserialize<GitHubRelease>(json);
-
-            // Remove the "commit-" prefix from the tag
-            var tag = release?.TagName;
-            if (tag != null && tag.StartsWith("sha1-")) tag = tag.Substring("sha1-".Length);
-
-            return tag;
+            await DownloadLatestRelease(repoOwner, repoName, latestTag, progressBar);
+            return GenericResult.Ok("Downloading latest release..");
         }
+
+        return GenericResult.Ok("Newest Version already!");
+    }
+
+
+    /// <summary>
+    ///     Gets the latest release tag from a GitHub repository.
+    /// </summary>
+    /// <param name="repoOwner">The owner of the repository.</param>
+    /// <param name="repoName">The name of the repository.</param>
+    /// <returns>The latest release tag, or null if the repository does not exist.</returns>
+    public static async Task<string?> GetLatestReleaseTagAsync(string repoOwner, string repoName)
+    {
+        var url = $"https://api.github.com/repos/{repoOwner}/{repoName}/releases/latest";
+        Client.DefaultRequestHeaders.UserAgent
+            .ParseAdd("Authaas-Docker-Updater"); // GitHub API requires a User-Agent header
+        var json = await Client.GetStringAsync(url);
+        var release = JsonSerializer.Deserialize<GitHubRelease>(json);
+
+        // Remove the "commit-" prefix from the tag
+        var tag = release?.TagName;
+        if (tag != null && tag.StartsWith("sha1-")) tag = tag.Substring("sha1-".Length);
+
+        return tag;
     }
 }
--- Authaas Docker/Services/IniFile.cs
+++ Authaas Docker/Services/IniFile.cs
@@ -16,11 +16,24 @@ public class IniFile
     private static extern int GetPrivateProfileString(string section, string key, string @default, StringBuilder retVal,
         int size, string filePath);
 
+    /// <summary>
+    /// Constructor for IniFile class.
+    /// </summary>
+    /// <param name="iniPath">Optional path to the ini file.</param>
+    /// <returns>
+    /// An instance of the IniFile class.
+    /// </returns>
     public IniFile(string? iniPath = null)
     {
         _path = new FileInfo(iniPath ?? _exe + ".ini").FullName;
     }
 
+    /// <summary>
+    /// Reads a value from the specified section and key in the ini file.
+    /// </summary>
+    /// <param name="key">The key to read from.</param>
+    /// <param name="section">The section to read from. If null, the exe name is used.</param>
+    /// <returns>The value read from the ini file.</returns>
     public string Read(string key, string? section = null)
     {
         var retVal = new StringBuilder(255);
@@ -28,21 +41,42 @@ public class IniFile
         return retVal.ToString();
     }
 
+    /// <summary>
+    /// Writes a key/value pair to the INI file.
+    /// </summary>
+    /// <param name="key">The key to write.</param>
+    /// <param name="value">The value to write.</param>
+    /// <param name="section">The section to write the key/value pair to. If null, the executable name is used.</param>
     public void Write(string key, string value, string? section = null)
     {
         WritePrivateProfileString(section ?? _exe, key, value, _path);
     }
 
+    /// <summary>
+    /// Deletes the specified key from the configuration file.
+    /// </summary>
+    /// <param name="key">The key to delete.</param>
+    /// <param name="section">The section to delete the key from. If null, the key will be deleted from the default section.</param>
     public void DeleteKey(string key, string? section = null)
     {
         Write(key, null, section ?? _exe);
     }
 
+    /// <summary>
+    /// Deletes a section from the executable file.
+    /// </summary>
+    /// <param name="section">The section to delete. If null, the entire executable file will be deleted.</param>
     public void DeleteSection(string? section = null)
     {
         Write(null, null, section ?? _exe);
     }
 
+    /// <summary>
+    /// Checks if a key exists in the configuration file.
+    /// </summary>
+    /// <param name="key">The key to check.</param>
+    /// <param name="section">The section to check in (optional).</param>
+    /// <returns>True if the key exists, false otherwise.</returns>
     public bool KeyExists(string key, string? section = null)
     {
         return Read(key, section).Length > 0;
